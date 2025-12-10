dd4b141c149587c7bd6eec35d798bb164f3b8cc2
Des Herriott <des.herriott@gmail.com>
2025-12-10 15:44:53 +0000

    fix: allow FTBUltimineAPI#currentBlockSelection to work client-side
    https://github.com/FTBTeam/FTB-Mods-Issues/issues/1906

--- common/src/main/java/dev/ftb/mods/ftbultimine/FTBUltimineCommon.java
+++ common/src/main/java/dev/ftb/mods/ftbultimine/FTBUltimineCommon.java
@@ -2,10 +2,17 @@ package dev.ftb.mods.ftbultimine;
 
 import net.minecraft.core.BlockPos;
 import org.jspecify.annotations.Nullable;
+import net.minecraft.world.entity.player.Player;
 
+import java.util.Collection;
 import java.util.List;
 
 public class FTBUltimineCommon {
 	public void setShape(int shapeIdx, @Nullable List<BlockPos> blocks) {
+	}
+
+	@Nullable
+	public Collection<BlockPos> getSelectedBlocks(Player player) {
+		return FTBUltimine.instance.getOrCreatePlayerData(player).cachedPositions();
 	}
 }
